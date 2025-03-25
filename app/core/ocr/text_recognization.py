e1d427e1808cabe2c643f1585cc7b7d9fd22095d
JibrilFlugel <138867313+JibrilFlugel@users.noreply.github.com>
2025-03-25 16:09:21 +0700

    Optimize PaddleOCR

--- app/core/ocr/text_recognization.py
+++ app/core/ocr/text_recognization.py
@@ -1,9 +1,9 @@
 from paddleocr import PaddleOCR
 
+ocr = PaddleOCR(use_angle_cls=True, use_gpu=False, cpu_threads=2) 
+
 def text_recognize(image):
-    ocr = PaddleOCR(use_angle_cls=True, use_gpu=True)
     res = ocr.ocr(image, cls=True)
     if res and res[0] and res[0][0]:
         return res[0][0][1]
     return None
-    
